34f6ef954b4e78f5898d24773899eca638dd3810
ycy190 <ycy190@163.com>
2021-07-22 12:21:42 +1200

    update app.js

--- cise-react-learn/src/App.js
+++ cise-react-learn/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Agile is about Values and Principles
+          Agile is about Values and Principles!
         </a>
       </header>
     </div>
